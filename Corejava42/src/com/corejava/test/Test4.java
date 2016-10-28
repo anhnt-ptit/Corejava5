e3b90766f5912f6ff11d0c981774d36243f3cee3
anhnt-ptit <anhnt.ptit@gmail.com>
2016-10-28 11:29:49 +0700

    This is commit 1
    This is commit 1

--- Corejava42/src/com/corejava/test/Test4.java
+++ Corejava42/src/com/corejava/test/Test4.java
@@ -5,6 +5,7 @@ public class Test4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test 4");
+		System.out.println("Hello Test 4");
 	}	
 
 }
